0070be81857047386b61b8073a457aca35b7c693
davidedileo <dudeda94@gmail.com>
2024-02-28 22:23:21 +0100

    feature: determina pari o dispari

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
     }
-    public static String determinaPariDispari (int num1){
+    public static String determinaPariODispari (int num1){
         if (num1 % 2 == 0){
             return "Pari";
         } else {

61cfc350a1dc5ecf503f3662ac897a352cc9b7ba
davidedileo <dudeda94@gmail.com>
2024-02-28 23:25:54 +0100

    feature: determina pari o dispari

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
     }
-    public static String determinaPariODispari (int num1){
-        if (num1 % 2 == 0){
+    //Per poter determinare se anche un numero decimale è pari o dispari, la funzione converte il parametro
+    // che passo alla funzione in stringa, dopodichè la funzione prende l'ultimo carattere di
+    //questa stringa che verrà riconvertito in numero.
+    public static String determinaPariODispari (double num1){
+        if (Character.getNumericValue((Double.toString(num1)).charAt(Double.toString(num1).length() - 1)) % 2 == 0){
             return "Pari";
         } else {
             return "Dispari";

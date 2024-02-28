ab788d5fd0b8baaa499db3152b7f182d61af5d92
Matteo Solinas <matteo.solinas91@gmail.com>
2024-02-28 22:57:47 +0100

    aggiungi feature: divisione

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import static java.lang.Double.NaN;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Calculator");
+    }
+
+    /*divide due numeri dati come input e restituisce il quoziente e il resto.
+      In caso il secondo numero inserito sia zero, restituisce NaN.
+     */
+    public static double[] divisione(double num1, double num2) {
+        double[] result = new double[2];
+        if (num2 != 0) {
+            result[0] = num1 / num2;
+            result[1] = num1 % num2;
+        } else {
+            result[0] = NaN;
+            result[1] = NaN;
+        }
+        return result;
     }
 
 }

68f4dfa8a95cfa34fa6c970d9d5ef97a940294f6
Draivanhoe <novacheaser@yahoo.it>
2024-02-29 11:22:26 +0100

    Aggiungi feature potenza

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
-public class Main {
+import java.util.concurrent.Callable;
 
+public class Main {
     public static void main(String[] args) {
         System.out.println("Calculator");
     }
 
+    public static double potenza (double base, int esponente) {
+        if (base == 0) {
+            if (esponente == 0) {
+                System.out.println("0 elevato a 0 non avrebbe senso, ma restituisco 0.");
+            }
+            return 0;
+        } else {
+            if (esponente == 0) {
+                return 1;
+            } else {
+                if (base == 1) {
+                    return 1;
+                } else {
+                    double risultato = base;
+
+                    if (esponente > 0) {
+                        for (int i = 1; i < esponente; i++) {
+                                risultato *= base;
+                        }
+
+                        return risultato;
+                    } else {
+                        for (int i = -1; i > esponente; i--) {
+                            risultato *= base;
+                        }
+
+                        return 1 / risultato;
+                    }
+                }
+            }
+        }
+    }
 }

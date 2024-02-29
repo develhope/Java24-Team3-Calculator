4c92d19ae230ae91d9706ad300ade3943dedc558
Draivanhoe <novacheaser@yahoo.it>
2024-02-29 17:11:32 +0100

    aggiungi flusso

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
+import java.io.Console;
+
 import static java.lang.Double.NaN;
+import static java.lang.Double.valueOf;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Calculator");
+        while (true) {
+            String[] operazioni = {"1) Somma (+)",
+                    "2) Sottrazione (-)",
+                    "3) Moltiplicazione (*)",
+                    "4) Divisione (/)",
+                    "5) Potenza (^)",
+                    "6) Pari o dispari (%)",
+                    "0) Termina programma"};
+
+            System.out.println("Seleziona l'operazione che vuoi eseguire scrivendo il rispettivo numero e premendo INVIO.");
+            for (String operazione : operazioni) {
+                System.out.println(operazione);
+            }
+
+            System.out.println("RISULTATO: " + scegliOperazione(chiediInputMenu()));
+        }
     }
 
     /*divide due numeri dati come input e restituisce il quoziente e il resto.
@@ -24,16 +41,18 @@ public class Main {
     //Per poter determinare se anche un numero decimale è pari o dispari, la funzione converte il parametro
     // che passo alla funzione in stringa, dopodichè la funzione prende l'ultimo carattere di
     //questa stringa che verrà riconvertito in numero.
-    public static String determinaPariODispari (double num1){
-        if (Character.getNumericValue((Double.toString(num1)).charAt(Double.toString(num1).length() - 1)) % 2 == 0){
+    public static String determinaPariODispari(double num1) {
+        if (Character.getNumericValue((Double.toString(num1)).charAt(Double.toString(num1).length() - 1)) % 2 == 0) {
             return "Pari";
         } else {
             return "Dispari";
         }
     }
+
     public static double sottrazione(double num1, double num2) {
         return num1 - num2;
     }
+
     public static double somma(double num1, double num2) {
         return num1 + num2;
     }

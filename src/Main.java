cda49a7ef3f9084e46bbbbead10c787d8f25722b
Chiara De Guglielmo <154986750+CDG01@users.noreply.github.com>
2024-02-29 15:00:46 +0100

    fix: ho risolto il conflitto

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,7 @@ public class Main {
         }
         return result;
     }
+
     //Per poter determinare se anche un numero decimale è pari o dispari, la funzione converte il parametro
     // che passo alla funzione in stringa, dopodichè la funzione prende l'ultimo carattere di
     //questa stringa che verrà riconvertito in numero.

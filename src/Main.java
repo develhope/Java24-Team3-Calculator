56dba4dd9faafcb1f6075682d3c754d5da848638
Draivanhoe <novacheaser@yahoo.it>
2024-03-05 17:54:33 +0100

    correggi metodo potenza, pulisci codice

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
-import static java.lang.Double.NaN;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         double[] ultimoRisultato = {0, 0};
         int selezioneMenu;
+        String avvisoNumeriDecimali = "Ricorda: per scrivere un numero decimale, utilizza la virgola e non il punto.";
 
         while (true) {
             String[] operazioni = {"1) SOMMA (+)",
@@ -27,10 +27,11 @@ public class Main {
             switch (selezioneMenu) {
                 case 0:
                     System.out.println("Termino il programma.");
-                    System.exit(0);
+                    return;
 
                 case 1:
-                    System.out.print("SOMMA\nInserisci il primo addendo: ");
+                    System.out.println("SOMMA\n" + avvisoNumeriDecimali);
+                    System.out.print("Inserisci il primo addendo: ");
                     inputOperazioni[0] = richiediInput();
                     System.out.print("Inserisci il secondo addendo: ");
                     inputOperazioni[1] = richiediInput();
@@ -39,7 +40,8 @@ public class Main {
                     break;
 
                 case 2:
-                    System.out.print("SOTTRAZIONE\nInserisci il minuendo: ");
+                    System.out.println("SOTTRAZIONE\n" + avvisoNumeriDecimali);
+                    System.out.print("Inserisci il minuendo: ");
                     inputOperazioni[0] = richiediInput();
                     System.out.print("Inserisci il sottraendo: ");
                     inputOperazioni[1] = richiediInput();
@@ -48,7 +50,8 @@ public class Main {
                     break;
 
                 case 3:
-                    System.out.print("MOLTIPLICAZIONE\nInserisci il primo fattore: ");
+                    System.out.println("MOLTIPLICAZIONE\n" + avvisoNumeriDecimali);
+                    System.out.print("Inserisci il primo fattore: ");
                     inputOperazioni[0] = richiediInput();
                     System.out.print("Inserisci il secondo fattore: ");
                     inputOperazioni[1] = richiediInput();
@@ -57,7 +60,8 @@ public class Main {
                     break;
 
                 case 4:
-                    System.out.print("DIVISIONE\nInserisci il dividendo: ");
+                    System.out.println("DIVISIONE\n" + avvisoNumeriDecimali);
+                    System.out.print("Inserisci il dividendo: ");
                     inputOperazioni[0] = richiediInput();
                     System.out.print("Inserisci il divisore: ");
                     inputOperazioni[1] = richiediInput();
@@ -66,16 +70,18 @@ public class Main {
                     break;
 
                 case 5:
-                    System.out.print("POTENZA\nInserisci la base: ");
+                    System.out.println("POTENZA\n" + avvisoNumeriDecimali);
+                    System.out.print("Inserisci la base: ");
                     inputOperazioni[0] = richiediInput();
-                    System.out.print("Inserisci l'esponente: ");
+                    System.out.print("Inserisci l'esponente (l'esponente deve essere un numero intero, altrimenti verrà arrotondato per difetto all'intero più vicino: ");
                     inputOperazioni[1] = richiediInput();
                     ultimoRisultato[0] = potenza(inputOperazioni[0], (int) inputOperazioni[1]);
-                    System.out.println(inputOperazioni[0] + " ^ " + inputOperazioni[1] + " = " + ultimoRisultato[0]);
+                    System.out.println(inputOperazioni[0] + " ^ " + (int) inputOperazioni[1] + " = " + ultimoRisultato[0]);
                     break;
 
                 case 6:
-                    System.out.print("PARI O DISPARI\nInserisci un numero: ");
+                    System.out.println("PARI O DISPARI\n" + avvisoNumeriDecimali);
+                    System.out.print("Inserisci un numero: ");
                     inputOperazioni[0] = richiediInput();
                     System.out.println(determinaPariODispari(inputOperazioni[0]));
                     break;
@@ -88,49 +94,59 @@ public class Main {
         }
     }
 
-    public static double[] divisione(double num1, double num2) {
-        /*
-        divide due numeri dati come input e restituisce il quoziente e il resto.
-        In caso il secondo numero inserito sia zero, restituisce NaN.
-        */
+    public static double[] divisione (double num1, double num2) {
+        // Divide due numeri dati come input e restituisce il quoziente e il resto.
+        // In caso di divisione per 0, avvisa l'utente e restituisce sia quoziente che resto uguali a 0.
+
         double[] result = new double[2];
         if (num2 != 0) {
             result[0] = num1 / num2;
             result[1] = num1 % num2;
         } else {
-            result[0] = NaN;
-            result[1] = NaN;
+            System.out.println("Impossibile dividere per 0.");
         }
         return result;
     }
 
-    //Per poter determinare se anche un numero decimale è pari o dispari, la funzione converte il parametro
-    // che passo alla funzione in stringa, dopodichè la funzione prende l'ultimo carattere di
-    //questa stringa che verrà riconvertito in numero.
-    public static String determinaPariODispari(double num1) {
-        if (Character.getNumericValue((Double.toString(num1)).charAt(Double.toString(num1).length() - 1)) % 2 == 0) {
-            return "Pari";
+    public static String determinaPariODispari (double num1) {
+        // Se il numero è intero, lo divide per due e stabilisce se questo è pari o dispari in base al resto.
+        // Invece, in caso di numero decimale, la funzione converte il numero
+        // in una stringa e legge il suo ultimo carattere per riconvertirlo in numero intero ed effettuare il controllo.
+
+        if (num1 == (int) num1) {
+            if (num1 % 2 == 0) {
+                return "Pari";
+            } else {
+                return "Dispari";
+            }
+
         } else {
-            return "Dispari";
+
+            if (Character.getNumericValue((Double.toString(num1)).charAt(Double.toString(num1).length() - 1)) % 2 == 0) {
+                return "Pari";
+            } else {
+                return "Dispari";
+            }
         }
+
     }
 
-    public static double sottrazione(double num1, double num2) {
+    public static double sottrazione (double num1, double num2) {
         return num1 - num2;
     }
 
-    public static double somma(double num1, double num2) {
+    public static double somma (double num1, double num2) {
         return num1 + num2;
     }
 
-    public static double moltiplicazione(double num1, double num2) {
+    public static double moltiplicazione (double num1, double num2) {
         return num1 * num2;
     }
 
     public static double potenza (double base, int esponente) {
         if (base == 0) {
             if (esponente == 0) {
-                System.out.println("0 elevato a 0 non avrebbe senso, ma restituisco 0.");
+                System.out.println("Impossibile elevare 0 a se stesso.");
             }
             return 0;
         } else {
@@ -162,7 +178,7 @@ public class Main {
 
     public static double richiediInput () {
         // A causa della localizzazione (non so se di IntelliJ o del sistema operativo
-        // vengono considerati valori corretti i numeri scritti utilizzando la virgola (esempio: 2,13)
+        // vengono considerati come valori corretti solo i numeri scritti utilizzando la virgola (esempio: 2,13)
         // mentre sono errati quelli con il punto (esempio: 2.13)
         double input = -1;
         boolean correctInput = false;
